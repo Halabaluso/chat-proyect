a21c1a110ed5fb6e7e6e93fab06efb84fdcb91a0
Halabaluso <dmartindiaz3@gmail.com>
2024-01-30 14:38:01 +0100

    --allow-empty

--- components/commons/tabs.vue
+++ components/commons/tabs.vue
@@ -4,14 +4,14 @@
             <template v-if="i === 0">
                 <input checked type="radio" name="my_tabs_2" role="tab" class="tab min-w-28" :aria-label="`${slot.name}`"
                     />
-                <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6 ">
+                <div role="tabpanel" class="tab-content py-5">
                     <slot :name = "slot.slot"></slot>
                 </div>
             </template>
             <template v-else>
-                <input type="radio" name="my_tabs_2" role="tab" class="tab min-w-28" :aria-label="`${slot.name}`"
+                <input type="radio" name="my_tabs_2" role="tab" class="tab" :aria-label="`${slot.name}`"
                     />
-                <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+                <div role="tabpanel" class="tab-content w-full py-5">
                     <slot :name = "slot.slot"></slot>
                 </div>
             </template>
